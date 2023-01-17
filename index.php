d0b0f49a181b2a17e89f4b9c89708037e6c6c84f
Gaudencia <conny@students.uonbi.ac.ke>
2023-01-17 13:55:20 +0300

    question three and four added

--- index.php
+++ index.php
@@ -92,3 +92,71 @@ $charges=$price + $vat + $additional
 $sql = $conn->prepare("INSERT INTO garbage (client_name, region, charges) VALUES (?, ?, ?)");
 $sql->bind_param("sss", $client_number, $region, $charges);
 
+<!--  web appliction forcustomer account-->
+$servername='localhost'
+$username='username'
+$password='password'
+$dbName='myDb'
+
+$conn=new mysqli($servername,$username,$password,$dbName)
+if(conn-> connect_error){
+    die('connection failed': .$conn->connect_error )
+    
+}
+$account_number=$_POST['account-number']
+<!-- $customer_number=customer_number
+$name=name
+$bank_balance=bank-balance -->
+$sql='SELECT name,bank_balance';
+$result=$conn->query($sql);
+if($result->num_rows >0){
+    while($row=$result->fetch_assoc()){
+        echo "name: " $row['name']. "bank_balance: " . $row['bank_balance'].
+    }
+}
+else{
+    echo "no search account"
+}
+<!-- web application for mosmos accomodation services -->
+$servername='localhost'
+$username='username'
+$password='password'
+$dbName='myDb'
+
+$conn=new mysqli($servername,$username,$password,$dbName)
+if(conn-> connect_error){
+    die('connection failed': .$conn->connect_error )
+    
+}
+$client_name=$_POST['client-name']
+$client_type=$_POST['client-type']
+$client_day=$_POST['client-day']
+function discount(){
+    discount=0
+    if(isset($client_type && $client_type= "regular")){
+        discount= 5200 - (15/100 * 5200)
+    }
+    elseif(isset($client_type && $client_type= "regular")){
+        if($client_day >= 3){
+          discount= 5200 - (15/100 * 5200)
+        }
+        
+    }
+  else
+  return discount
+}
+<!-- total charges -->
+function charges(){
+$charges=0;
+if(isset($client_type && $client_type= "regular')){
+    charges=5200
+}
+elseif(isset($client_type && $client_type="new-client"){
+    6700
+}
+return charges
+}
+
+$total_charges=charge -discount
+$sql = "INSERT accomodation (client_name,client_type,client_day, charges,discount,total_charges)
+VALUES ($client_name,$client_type,$client_day, $charges,$discount,$total_charges)";
